af183d55b6ab35988bb6eba8e6077e5a78e20b26
Dimitrispol99 <dimitrispoluzos99@hotmail.com>
2026-03-31 23:05:18 +0300

    Initial Commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Dimitris Poluzos on 31/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
